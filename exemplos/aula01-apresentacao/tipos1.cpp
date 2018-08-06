495fad5a2992718f6687449fd0131389f0bd7b2e
Flavio Figueiredo <flaviovdf@dcc.ufmg.br>
2018-08-06 16:39:53 -0300

    Precision c++

--- exemplos/aula01-apresentacao/tipos1.cpp
+++ exemplos/aula01-apresentacao/tipos1.cpp
@@ -1,12 +1,18 @@
+#include <iomanip>
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main() {
-  int x = 7;
+  double pi = 3.1415;
   cout << "Olá DCC :) ";
-  cout << "O valor de x é? ";
-  cout << 7;
+  cout << "O valor de pi é? ";
+  cout << pi;
   cout << endl;
+  cout << "E se eu quiser uma precisão menor? ";
+  cout << setprecision(1) << pi;
+  cout << endl;
+  cout << "Pi ao quadrado com 7 precisao: " << setprecision(7) << pow(pi, 2);
   return 0;
 }
