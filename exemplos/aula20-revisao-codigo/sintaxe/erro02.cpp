84e49a4a7e04757ea7a0e1499103c7ff4b1c6c4f
Flavio Figueiredo <flaviovdf@gmail.com>
2018-08-08 06:32:24 -0300

    Adicionando a aula20

---
+++ exemplos/aula20-revisao-codigo/sintaxe/erro02.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+int main() {
+  std::unordered_map<std::string, int> nome2mat = {};
+  nome2mat.insert("Flavio", "123")
+  return 0;
+}
