b43b1f438253086ada6f040b89de4a6ae164d4d1
Flavio Figueiredo <flaviovdf@gmail.com>
2018-08-15 11:35:49 -0300

    Update heap2.cpp

--- exemplos/aula02-memoria/heap2.cpp
+++ exemplos/aula02-memoria/heap2.cpp
@@ -9,11 +9,11 @@ int *cria_um_vetor_zerado(int n) {
 }
 
 int main() {
-    std::cout << "Heap" << std::endl;
-    int *vetor_heap = cria_um_vetor_zerado(10);
-    vetor_heap[0] = 10;
-    for (int i = 0; i < 10; i++) {
-        std::cout << vetor_heap[i] << std::endl;
-    }
-    delete[] vetor_heap;
+  std::cout << "Heap" << std::endl;
+  int *vetor_heap = cria_um_vetor_zerado(10);
+  vetor_heap[0] = 10;
+  for (int i = 0; i < 10; i++) {
+    std::cout << vetor_heap[i] << std::endl;
+  }
+  delete[] vetor_heap;
 }
