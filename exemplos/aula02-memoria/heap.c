8c89ff7547641343555de118980ef16a2524d8ca
Flavio Figueiredo <flaviovdf@gmail.com>
2018-08-15 11:33:40 -0300

    Update heap.c

--- exemplos/aula02-memoria/heap.c
+++ exemplos/aula02-memoria/heap.c
@@ -2,10 +2,10 @@
 
 //C
 int *cria_um_vetor_zerado_c(int n) {
-    int *rv = (int *) malloc(n * sizeof(int));
-    for (int i = 0; i < n; i++)
-        rv[i] = 0;
-    return rv;
+  int *rv = (int *) malloc(n * sizeof(int));
+  for (int i = 0; i < n; i++)
+    rv[i] = 0;
+  return rv;
 }
 
 int main() {
