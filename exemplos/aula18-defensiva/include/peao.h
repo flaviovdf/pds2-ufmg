7c2e799b5ccec4bdff5cb813486e4e4c0caaf115
Flavio Figueiredo <flaviovdf@gmail.com>
2018-11-01 08:23:19 -0200

    Corrigindo nomes

---
+++ exemplos/aula18-defensiva/include/peao.h
@@ -0,0 +1,8 @@
+#ifndef PDS2_PEAO_H
+#define PDS2_PEAO_H
+
+#include "peca.h"
+
+
+
+#endif
--- exemplos/aula18-defensiva/src/rainha.cpp
+++ exemplos/aula18-defensiva/src/rainha.cpp
@@ -1,15 +1,1 @@
 #include "rainha.h"
-
-Rainha::Rainha(int x, int y): _x(x), _y(y) {}
-
-int Rainha::get_x() {
-  return _x;
-}
-
-int Rainha::get_y() {
-  return _y;
-}
-
-bool Rainha::pode_mover(int x, int y) {
-  return false;
-}
