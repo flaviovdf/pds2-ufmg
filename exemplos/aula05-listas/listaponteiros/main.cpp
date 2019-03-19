f3df1bdb4185dc3b326d4b242d29ad389cf25cd7
Flavio Figueiredo <flaviovdf@gmail.com>
2019-03-19 18:29:28 -0300

    Atualizando exemplos

---
+++ exemplos/aula05-listas/listaponteiros/main.cpp
@@ -0,0 +1,12 @@
+#include <string>
+
+#include "listasimples.h"
+
+int main(void) {
+  ListaSimplesmenteEncadeada lista = ListaSimplesmenteEncadeada();
+  lista.inserir_elemento(2);
+  lista.imprimir();
+  lista.remove_iesimo(0);
+  lista.imprimir();
+  return 0;
+}
